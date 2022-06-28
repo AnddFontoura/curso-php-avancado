4d0ba7fd032905e71c4be9270b303ac0ad60ba2a
André Fontoura <andd.fontoura@hotmail.com>
2022-06-28 10:35:34 -0300

    Add Delete

--- app/classes/CategoryClass.php
+++ app/classes/CategoryClass.php
@@ -3,11 +3,26 @@
 namespace App\classes;
 
 use App\classes\model\CategoryModel;
+use PDO;
 
 class CategoryClass extends ControllerClass {
     protected $table = "categories";
     protected $viewDirectory = 'category';
     protected $model;
+    protected $uploadFolder = 'upload/category';
+    protected $uploadParameter = 'image';
+    protected $uploadColumnName = 'image';
+    protected $allowedFileType = [
+        'image/jpeg',
+        'image/png',
+        'image/bmp',
+    ];
+    protected $allowedFileExtension = [
+        'jpg',
+        'png',
+        'jpeg',
+        'bmp',
+    ];
 
     function __construct()
     {
@@ -15,4 +30,51 @@ class CategoryClass extends ControllerClass {
 
         $this->model = new CategoryModel();
     }
+
+    public function delete()
+    {
+        if (!isset($_POST['id'])) {
+            $return = [
+                'message' => 'Informação enviada não está de acordo com o esperado. Requisição POST'
+            ];
+
+            header('Content-Type: application/json');
+            header ('HTTP/1.1 405 Method Not Allowed');
+            echo json_encode($return);
+            die();
+        }
+
+        $idCategory = $_POST['id'] ?? 0;
+
+        $query = $this->dbConnection->prepare("
+            SELECT
+                *
+            FROM
+                categories
+            WHERE
+                id = :idCategory        
+        ");
+        $query->bindParam(':idCategory', $idCategory);
+        $query->execute();
+        $resultSelect = $query->fetch(PDO::FETCH_ASSOC);
+        
+        if (!is_null($resultSelect)) {
+            $query = $this->dbConnection->prepare("
+                DELETE FROM 
+                    categories
+                WHERE
+                    id = :idCategory
+            ");
+            $query->bindParam(':idCategory', $idCategory);
+            $resultDelete = $query->execute();
+
+            $resultDelete ? $return = ['message' => 'Registro Deletado'] : $return = ['message' => 'Nenhum registro deletado'];
+            
+            echo json_encode($return);
+            die();
+        }
+
+        echo json_encode(['message' => 'Dado já foi deletado anteriormente']);
+    }
+
 }
---
+++ view/category/form.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+    <head>
+        <title> Formulário de envio de arquivo </title> 
+        <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+    </head>
+
+    <body class='container'>
+        <div class='row'>
+            <div class='col-12'>
+                <form action="../Category/save" method="POST" enctype="multipart/form-data">
+                    <div class="card">
+                        <div class="card-header">
+                            <h1> Criar Categoria </h1>
+                        </div>
+                    
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <span> Nome </span>
+                                        <input type="text" class="form-control" name="name"> </input>
+                                    </div>
+                                </div>
+
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <span> Arquivo </span>
+                                        <input type="file" class="form-control" name="image"> </input>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card-footer">
+                            <button type="submit"> Incluir </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+    <script src="../public/js/jquery.min.js"></script>
+    <script src="../public/js/bootstrap.min.js"></script>
+</body>
